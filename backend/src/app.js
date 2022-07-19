4f82e47f9597146426841f70437e2f400ef88344
jintak0401 <jintak0401@naver.com>
2022-07-20 07:21:04 +0900

    feat : app.js 에서 '/'를 글로벌 라우터와 연결
    app.js 에서 '/'을 /routes/apiRouter와 연결했습니다.
    
    \#6

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const apiRouter = require("./routes/apiRouter");
 
 const app = express();
 
@@ -9,8 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-	res.send('Hi');
-});
+app.use('/', apiRouter);
 
 module.exports = app;
