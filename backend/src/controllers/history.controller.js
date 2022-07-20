ad0c850810eee2f379c154ce41f7a37fdff37c0f
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-07-20 21:13:46 +0900

    feat: history router test code 생성 #11
    - class 문법을 통한 HistoryController 생성

---
+++ backend/src/controllers/history.controller.js
@@ -0,0 +1,21 @@
+const historyService = require('../services/history.service');
+
+class HistoryController {
+	#historyService;
+	constructor(historyService) {
+		this.#historyService = historyService;
+	}
+
+	createHistory = async (req, res) => {
+		const data = req.body;
+
+		// const result = await this.#historyService.createHistory(data);
+
+		res.status(201).json({
+			result,
+		});
+	};
+}
+const historyController = new HistoryController(historyService);
+
+module.exports = { HistoryController, historyController };
