6e2a078206587160fafd9f543078147246d4d790
jintak0401 <jintak0401@naver.com>
2022-07-22 08:46:12 +0900

    feat: HistoryService.createHistory를 사용가능하도록 수정 #11
    - 임시 값이 아닌 실제 쿼리를 수행할 수 있도록 인자를 받아 쿼리문을 수행합니다.
    - INSERT 수행 결과를 result로 반환합니다.

--- backend/src/services/history.service.js
+++ backend/src/services/history.service.js
@@ -4,22 +4,29 @@ const pool = require("../db");
 class HistoryService {
   constructor() {}
 
-  async createHistory(data) {
+  async createHistory({
+    userId,
+    date,
+    direction,
+    category,
+    description,
+    paymentId,
+    amount,
+  }) {
     const connection = await pool.getConnection();
     try {
-      const [row, column] = await connection.query(CREATE_HISTORY, [
-        1,
-        "2022-07-10",
-        "in",
-        "Test",
-        "테스트입니다.",
-        1,
-        10000,
+      const [result] = await connection.query(CREATE_HISTORY, [
+        userId,
+        date,
+        direction,
+        category,
+        description,
+        paymentId,
+        amount,
       ]);
-
       await connection.commit();
 
-      return { row, column };
+      return { ok: true, result };
     } catch (error) {
       await connection.rollback();
       return { ok: false, error };
