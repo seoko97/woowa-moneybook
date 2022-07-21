c1cafe9e6923d0f0c4c1de28d7e11f5f6c1e84b1
jintak0401 <jintak0401@naver.com>
2022-07-22 08:16:56 +0900

    feat: 사용자의 결제수단들을 읽는 READ_PAYMENTS 쿼리문 추가 #11
    - [ userId ]를 입력받습니다.
    - { title: string, paymentId: int } 배열 형태로 읽어들입니다.

--- backend/src/constant/queries.js
+++ backend/src/constant/queries.js
@@ -2,6 +2,9 @@ const QUERIES = {
   // [ 유저 id, 일자, 수입/지출, 카테고리, 설명, 결제수단 id, 수량 ]
   CREATE_HISTORY:
     "INSERT INTO TRANSACTION_TB(userId, trxDate, direction, category, description, paymentId, amount) VALUES (?, ?, ?, ?, ?, ?, ?)",
+  // [ 유저 id ]
+  READ_PAYMENTS:
+    "SELECT PAY.title, PAY.id AS paymentId FROM PAYMENT_TB AS PAY JOIN USER_PAYMENT_TB AS USER_PAY ON PAY.id = USER_PAY.paymentId WHERE USER_PAY.userId = ?",
 };
 
 module.exports = QUERIES;
