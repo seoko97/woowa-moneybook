6ef0fb38a1ef696d40fbeffc4e9984e148435781
jintak0401 <jintak0401@naver.com>
2022-07-22 08:48:35 +0900

    feat: HistoryService.getHistories 구현 #11
    - { userId, year, month, direction, category } 를 입력받습니다. 이중 userId, year, month는 필수이며, direction, category는 선택입니다.
    - direction, category를 입력하지 않으면 와일드카드가 입력됩니다.

--- backend/src/services/history.service.js
+++ backend/src/services/history.service.js
@@ -1,8 +1,30 @@
-const { CREATE_HISTORY } = require("../constant/queries");
+const { CREATE_HISTORY, READ_HISTORIES } = require("../constant/queries");
 const pool = require("../db");
 
 class HistoryService {
   constructor() {}
+
+  // 필수: { userId, year, month }
+  // 선택: { direction, category }
+  async getHistories({ userId, year, month, direction = "%", category = "%" }) {
+    const connection = await pool.getConnection();
+    try {
+      // [ 유저 id, 년도, 달, 수입/지출, 카테고리]
+      const [trxList] = await connection.query(READ_HISTORIES, [
+        userId,
+        year,
+        month,
+        direction,
+        category,
+      ]);
+      return { ok: true, trxList };
+    } catch (error) {
+      await connection.rollback();
+      return { ok: false, error };
+    } finally {
+      await connection.release();
+    }
+  }
 
   async createHistory({
     userId,
