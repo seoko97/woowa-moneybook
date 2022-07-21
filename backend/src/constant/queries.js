4ecb7db8a29f5771f7d4b70c1d56d17ef08ca1e9
jintak0401 <jintak0401@naver.com>
2022-07-22 08:14:03 +0900

    fix: CREATE_HISTORY 쿼리문 오류 수정
    - date 에서 trxDate로 수정했습니다. mysql의 date 타입이 있어서 충돌이 일어납니다.
    - VALUE에서 VALUES로 수정

--- backend/src/constant/queries.js
+++ backend/src/constant/queries.js
@@ -1,6 +1,7 @@
 const QUERIES = {
-	CREATE_HISTORY:
-		'INSERT INTO TRANSACTION_TB(userId, date, direction, category, description, paymentId, amount) VALUE (?, ?, ?, ?, ?, ?, ?)',
+  // [ 유저 id, 일자, 수입/지출, 카테고리, 설명, 결제수단 id, 수량 ]
+  CREATE_HISTORY:
+    "INSERT INTO TRANSACTION_TB(userId, trxDate, direction, category, description, paymentId, amount) VALUES (?, ?, ?, ?, ?, ?, ?)",
 };
 
 module.exports = QUERIES;
