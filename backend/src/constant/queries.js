3ff814e9dcbc123b4faa8023b9cea7297d0dff47
jintak0401 <jintak0401@naver.com>
2022-07-22 08:32:09 +0900

    feat: USER_PAYMENT_TB에 사용자의 결제수단을 매핑해주는 쿼리문 작성 #11
    - [ userId, paymentId ]를 입력받습니다.
    - PAYMENT_TB의 결제수단과 userId를 이용해 사용자를 매핑해줍니다.

--- backend/src/constant/queries.js
+++ backend/src/constant/queries.js
@@ -7,6 +7,9 @@ const QUERIES = {
     "SELECT PAY.title, PAY.id AS paymentId FROM PAYMENT_TB AS PAY JOIN USER_PAYMENT_TB AS USER_PAY ON PAY.id = USER_PAY.paymentId WHERE USER_PAY.userId = ?",
   // [ 결제수단 이름 ]
   CREATE_PAYMENT: "INSERT INTO PAYMENT_TB (title) VALUES (?)",
+  // [ 유저 id, 결제수단 id]
+  CREATE_USER_PAYMENT:
+    "INSERT INTO USER_PAYMENT_TB (userId, paymentId) VALUES (?, ?)",
 };
 
 module.exports = QUERIES;
