58a5d0dee276f1ef6084b4f515af7a8d0f37adb9
jintak0401 <jintak0401@naver.com>
2022-07-22 22:15:13 +0900

    feat: PaymentService.getPaymentByTitle 구현 #11
    - PAYMENT_TB에 입력받은 title을 가지는 레코드를 검색해주는 함수입니다.
    - 만약 있다면 paymentItem에 해당 결제수단의 { title: string, id: number }이, 없다면 null을 반환해줍니다.

--- backend/src/services/payment.service.js
+++ backend/src/services/payment.service.js
@@ -3,6 +3,7 @@ const {
   DELETE_PAYMENT,
   CREATE_USER_PAYMENT,
   CREATE_PAYMENT,
+  READ_PAYMENT_BY_TITLE,
 } = require("../constant/queries");
 const pool = require("../db");
 
@@ -81,7 +82,7 @@ class PaymentService {
    * @async
    * @function createPayment
    * @param {string} title
-   * @return {Promise<{paymentItem: {id: number, title: string}, ok: true}|{ok: false, error:Error}>}
+   * @return {Promise<{paymentItem: PaymentItem, ok: true}|{ok: false, error:Error}>}
    * @description PAYMENT_TB에 title을 가지는 새로운 레코드 생성해주는 함수
    */
   async createPayment({ title }) {
@@ -96,6 +97,30 @@ class PaymentService {
       return {
         ok: true,
         paymentItem,
+      };
+    } catch (error) {
+      await connection.rollback();
+      return { ok: false, error };
+    } finally {
+      await connection.release();
+    }
+  }
+
+  /**
+   * @async
+   * @function getPaymentByTitle
+   * @param {string} title
+   * @return {Promise<{paymentItem: (PaymentItem|null), ok: true}|{ok: false, error: Error}>}
+   * @description title로 PAYMENT_TB에 동일한 이름을 가진 결제수단이 있는지 확인하는 함수
+   */
+  async getPaymentByTitle({ title }) {
+    const connection = await pool.getConnection();
+    try {
+      // [ 결제수단 이름 ]
+      const [payment] = await connection.query(READ_PAYMENT_BY_TITLE, [title]);
+      return {
+        ok: payment.length !== 0,
+        paymentItem: payment.length !== 0 ? payment[0] : null,
       };
     } catch (error) {
       await connection.rollback();
