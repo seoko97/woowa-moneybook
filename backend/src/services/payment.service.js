b8a4bf66e253409995bdd9213dfd8f37f86dc016
jintak0401 <jintak0401@naver.com>
2022-07-22 22:00:56 +0900

    feat: PaymentService.deletePayment 구현 #11
    - USER_PAYMENT_TB에서 userId와 paymentId가 매핑되어 있는 레코드를 삭제해줍니다.
    - { userId, paymentId }를 입력받습니다.
    - 성공 실패여부를 ok로 알려줍니다.
    - 실패시 error도 return 해줍니다.

--- backend/src/services/payment.service.js
+++ backend/src/services/payment.service.js
@@ -12,7 +12,7 @@ class PaymentService {
    * @async
    * @function getPaymentList
    * @param {number} id - userId
-   * @return {Promise<{ok: false, error}|{paymentList: PaymentItem[], ok: true}>}
+   * @return {Promise<{ok: false, error:Error}|{paymentList: PaymentItem[], ok: true}>}
    */
   async getPaymentList({ id }) {
     const connection = await pool.getConnection();
@@ -20,6 +20,27 @@ class PaymentService {
       const [paymentList] = await connection.query(READ_PAYMENTS, [id]);
       await connection.commit();
       return { ok: true, paymentList };
+    } catch (error) {
+      await connection.rollback();
+      return { ok: false, error };
+    } finally {
+      await connection.release();
+    }
+  }
+
+  /**
+   * @async
+   * @function deletePayment
+   * @param {number} userId
+   * @param {number} paymentId
+   * @return {Promise<{ok: boolean, error: Error|undefined}>}
+   */
+  async deletePayment({ userId, paymentId }) {
+    const connection = await pool.getConnection();
+    try {
+      // [ 유저 id, 결제수단 id ]
+      await connection.query(DELETE_PAYMENT, [userId, paymentId]);
+      return { ok: true };
     } catch (error) {
       await connection.rollback();
       return { ok: false, error };
