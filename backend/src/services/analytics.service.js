416c0ba1756ae104b742e28d490701d6a4774ca1
jintak0401 <jintak0401@naver.com>
2022-07-22 21:36:15 +0900

    feat: AnalyticsService.getTotalExpenditures 구현 #11
    - 카테고리별 월별 합계를 구하는 함수입니다.
    - { userId, year, category }를 입력받습니다.
    - { month: number, total: number } 의 배열을 반환합니다.

---
+++ backend/src/services/analytics.service.js
@@ -0,0 +1,45 @@
+const { READ_DETAIL } = require("../constant/queries");
+const pool = require("../db");
+
+/**
+ * @typedef {Object} TotalExpenditure
+ * @property {number} month
+ * @property {number} total
+ */
+
+/**
+ * @typedef {Object} GetTotalExpendituresReturn
+ * @property {TotalExpenditure[]} totalExpenditures
+ * @property {boolean} ok
+ */
+class AnalyticsService {
+  /**
+   * @async
+   * @function getTotalExpenditures
+   * @param {number} userId
+   * @param {number} year
+   * @param {string} category
+   * @return {GetTotalExpendituresReturn}
+   */
+  async getTotalExpenditures({ userId, year, category }) {
+    const connection = await pool.getConnection();
+    try {
+      // [ 유저 id, 년도, 카테고리 ]
+      const [detail] = await connection.query(READ_DETAIL, [
+        userId,
+        year,
+        category,
+      ]);
+      return { ok: true, detail };
+    } catch (error) {
+      await connection.rollback();
+      return { ok: false, error };
+    } finally {
+      await connection.release();
+    }
+  }
+}
+
+const analyticsService = new AnalyticsService();
+
+module.exports = analyticsService;
