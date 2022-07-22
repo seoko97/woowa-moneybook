72690031c24a24cc7dfebabcbe2dc2de16a9da55
jintak0401 <jintak0401@naver.com>
2022-07-23 01:50:03 +0900

    feat: trxListConvertMap 함수의 인자가 undefined 일 때 처리
    - 인자로 undefined를 받게 되면 undefined를 return해주도록 수정하였습니다.

--- backend/src/utils/trxListConvertMap.js
+++ backend/src/utils/trxListConvertMap.js
@@ -1,13 +1,12 @@
 const trxListConvertMap = (trxList) => {
-  const ret = trxList.reduce((acc, cur) => {
+  if (!trxList) return undefined;
+  return trxList.reduce((acc, cur) => {
     if (acc.length === 0 || acc.at(-1).date !== cur.date) {
       acc.push({ date: cur.date, trxList: [] });
     }
     acc.at(-1).trxList.push(cur);
     return acc;
   }, []);
-  console.log(ret);
-  return ret;
 };
 
 module.exports = trxListConvertMap;
