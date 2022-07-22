1c13252416257c95e1cdaf37d71159180852124c
jintak0401 <jintak0401@naver.com>
2022-07-23 02:58:04 +0900

    refactor: 사용하지 않는 detailRouter.js 삭제 #11

--- backend/src/routes/detailRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const detailController = require("../controllers/detailController");
-
-const detailRouter = express.Router();
-
-detailRouter.get("/", detailController.getDetail);
-
-module.exports = detailRouter;
