26827feb42fef87e6224efec710f987829088d5d
jintak0401 <jintak0401@naver.com>
2022-07-22 23:54:02 +0900

    feat: PaymentController.getPaymentList 구현 #11
    - 사용자가 등록한 모든 결제수단을 불러오는 함수입니다.
    - body로 { id: number }을 받습니다. id는 userId 입니다.
    - { title: string, id: number } 의 배열형태를 반환받습니다.

---
+++ backend/src/controllers/payment.controller.js
@@ -0,0 +1,21 @@
+const paymentService = require("../services/payment.service");
+
+class PaymentController {
+  #paymentService;
+  constructor(paymentService) {
+    this.#paymentService = paymentService;
+  }
+
+  /**
+   * @param {number} req.body.id - userId
+   * @return {Promise<{paymentList: PaymentItem[], ok: true}|{ok: false, error:Error}>}
+   */
+  getPaymentList = async (req, res) => {
+    const data = req.body;
+    const result = await this.#paymentService.getPaymentList(data);
+    res.status(200).json(result);
+  };
+}
+const paymentController = new PaymentController(paymentService);
+
+module.exports = { PaymentController, paymentController };
