34c965911ba4b28d3afdcb8acc80fb75f491b538
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-07-23 15:25:28 +0900

    feat: dom handler를 통한 Compoennt Handling - 기존 parent node를 입력받아 Component 내부에서 append 실행 - element 중간에 Component를 입력받을 수 없는 이슈 발생 - Component 생성시 target element를 반환 - instanceof 메서드를 통해 data type을 검사하여 element일경우 해당 node를 반환, append 실행 - #20

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,66 +1,61 @@
 import Component from "../../core/component";
-import { dispatchCutomEvent } from "../../utils/customEvent";
+import { dispatchCutomEvent } from "../../utils/customEventHandler";
 import AnalyticsIcon from "../../../public/analyticsIcon.svg";
 import CalendarIcon from "../../../public/calendarIcon.svg";
 import HistoryIcon from "../../../public/historyIcon.svg";
 import DateIndicator from "./DateIndicator";
+import { createElement, h } from "../../utils/domHandler";
+import Svg from "../../core/svg";
 import "../../styles/header.css";
 
 export default class Header extends Component {
   constructor() {
-    super("header");
+    super();
 
-    this.$target.classList.add("header");
-    this.$target.classList.add("template");
     this.render();
     this.setEvent();
+
+    return this.$target;
   }
 
   setEvent() {
     this.addEvent("click", "a", (e) => {
       e.preventDefault();
       const $a = e.target.closest("a");
+
       if (!$a) return;
+
       const href = $a.getAttribute("href");
+
       if (href === location.pathname) {
         history.replaceState(null, null, href);
       } else {
         history.pushState(null, null, href);
       }
+
       dispatchCutomEvent("locationChange", window);
     });
   }
+
   render() {
-    const $template = document.createElement("div");
-    $template.classList.add("template");
-    const $dateIndicator = new DateIndicator();
-
-    const $logo = document.createElement("div");
-    $logo.classList.add("header--logo");
-    $logo.innerHTML = `<a href="/">우아한 가계부</a>`;
-
-    const $nav = document.createElement("div");
-    $nav.classList.add("header--nav");
-    $nav.innerHTML = `
-        <a href="/">
-          ${HistoryIcon}
-        </a>
-        <a href="/calendar">
-          ${CalendarIcon}
-        </a>
-        <a href="/analytics">
-          ${AnalyticsIcon}
-        </a>
-    `;
-
-    $template.append($logo);
-    $template.append($dateIndicator);
-    $template.append($nav);
-
-    this.$target.append($template);
-  }
-
-  getElement() {
-    return this.$target;
+    this.$target = createElement(
+      h(
+        "header",
+        { class: "header" },
+        h(
+          "div",
+          { class: "template" },
+          h("div", { class: "header--logo" }, h("a", { href: "/" }, "우아한 가계부")),
+          new DateIndicator({ $parent: this }),
+          h(
+            "nav",
+            { class: "header--nav" },
+            new Svg("a", { href: "/" }, HistoryIcon),
+            new Svg("a", { href: "/calendar" }, CalendarIcon),
+            new Svg("a", { href: "/analytics" }, AnalyticsIcon)
+          )
+        )
+      )
+    );
   }
 }
