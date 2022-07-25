e02cb8d4d2c3f2153cb6f937bdb5b80595047a22
jintak0401 <jintak0401@naver.com>
2022-07-26 08:06:26 +0900

    feat: Svg 생성자에 svg 인자가 html Element도 수용하도록 변경 #27
    - 기존 코드는 Svg 이미지만 사용가능한 문제가 있습니다.
    - Svg element도 인자로 받을 수 있도록 수정했습니다.

--- frontend/src/core/svg.js
+++ frontend/src/core/svg.js
@@ -3,7 +3,8 @@ import { createElement, h } from "../utils/domHandler";
 export default class Svg {
   constructor(type, props, svg) {
     this.$element = createElement(h(type, props));
-    this.$element.innerHTML = svg;
+    if (typeof svg === "string") this.$element.innerHTML = svg;
+    else this.$element.innerHTML = svg.outerHTML;
     return this.$element;
   }
 }
