d88c25ee8331a3fc073d3fac71507e8d78ec71db
jintak0401 <jintak0401@naver.com>
2022-07-25 17:49:10 +0900

    feat: HistoryController.deleteHistory 구현 #24
    - 거래 내역을 삭제해주는 컨트롤러입니다.

--- backend/src/controllers/history.controller.js
+++ backend/src/controllers/history.controller.js
@@ -35,6 +35,11 @@ class HistoryController {
     const result = await this.#historyService.updateHistory(req.body);
     res.status(200).json(result);
   };
+
+  deleteHistory = async (req, res) => {
+    const result = await this.#historyService.deleteHistory(req.body);
+    res.status(200).json(result);
+  };
 }
 const historyController = new HistoryController(historyService);
 
