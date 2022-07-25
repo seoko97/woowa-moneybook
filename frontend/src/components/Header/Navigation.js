20b7da27ee34e8e9aabbb0e4390b7df1203708ba
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-07-25 11:14:04 +0900

    feat: navigation component 생성 - 기존 header 내부 생성되어 있던 nav 분리 - #19 - #23

--- frontend/src/components/Header/Navigation.js
+++ frontend/src/components/Header/Navigation.js
@@ -1,1 +1,30 @@
+import Svg from "../../core/svg";
+import Component from "../../core/component";
+import AnalyticsIcon from "../../../public/analyticsIcon.svg";
+import CalendarIcon from "../../../public/calendarIcon.svg";
+import HistoryIcon from "../../../public/historyIcon.svg";
+import { createElement, h } from "../../utils/domHandler";
 
+class Navigation extends Component {
+  constructor() {
+    super();
+
+    this.render();
+
+    return this.$target;
+  }
+
+  render() {
+    this.$target = createElement(
+      h(
+        "nav",
+        { class: "header--nav" },
+        new Svg("a", { href: "/" }, HistoryIcon),
+        new Svg("a", { href: "/calendar" }, CalendarIcon),
+        new Svg("a", { href: "/analytics" }, AnalyticsIcon)
+      )
+    );
+  }
+}
+
+export default Navigation;
