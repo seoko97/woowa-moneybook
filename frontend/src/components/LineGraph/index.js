71468c1f74ad0b8ce7a767d84d696a657afc84b4
jintak0401 <jintak0401@naver.com>
2022-07-26 08:22:32 +0900

    feat: 격자무늬를 그려주는 makeDivideLine 함수 구현 #27
    - 꺾은선 그래프 내 격자무늬를 그려주는 makeDivideLine 함수를 구현했습니다.

--- frontend/src/components/LineGraph/index.js
+++ frontend/src/components/LineGraph/index.js
@@ -12,20 +12,106 @@ export default class LineGraph extends Component {
     return this.$target;
   }
 
-  render() {
-    const width = 200;
-    const height = 100;
-    const widthLength = 600;
-    const heightLength = 400;
+  makeBorder({ viewBoxWidth, viewBoxHeight, borderColor }) {
+    return h("path", {
+      d: `M 0 0 H ${viewBoxWidth} V ${viewBoxHeight} H 0 Z`,
+      fill: "transparent",
+      stroke: borderColor,
+      ["stroke-width"]: "0.3",
+    });
+  }
 
+  makeDivideLine({
+    viewBoxWidth,
+    viewBoxHeight,
+    horizontalUnit,
+    verticalUnit,
+    divideLineColor,
+    duration,
+  }) {
+    const horizontal = Array.from({ length: horizontalUnit }).map((_, i) => {
+      return h(
+        "path",
+        {
+          d: `M 0 ${
+            viewBoxHeight * ((horizontalUnit - i) / (horizontalUnit + 1))
+          } H ${viewBoxWidth}`,
+          stroke: divideLineColor,
+          ["stroke-width"]: "0.3",
+          ["stroke-dashoffset"]: `${viewBoxWidth}`,
+          ["stroke-dasharray"]: `${viewBoxWidth}`,
+        },
+        duration &&
+          h("animate", {
+            attributeName: "stroke-dashoffset",
+            begin: `${duration * i * 0.1}`,
+            dur: `${duration}`,
+            from: `${viewBoxWidth}`,
+            to: "0",
+            fill: "freeze",
+            calcMode: "spline",
+            keySplines: "0.1 0.8 0.2 1;0.1 0.8 0.2 1",
+            keyTimes: "0;0.5;1",
+          })
+      );
+    });
+    const vertical = Array.from({ length: verticalUnit }).map((_, i) => {
+      return h(
+        "path",
+        {
+          d: `M ${viewBoxWidth * ((i + 1) / (verticalUnit + 1))} ${viewBoxHeight} V 0`,
+          stroke: divideLineColor,
+          ["stroke-width"]: "0.3",
+          ["stroke-dashoffset"]: `${viewBoxHeight}`,
+          ["stroke-dasharray"]: `${viewBoxHeight}`,
+        },
+        duration &&
+          h("animate", {
+            attributeName: "stroke-dashoffset",
+            begin: `${duration * i * 0.05}`,
+            dur: `${duration}`,
+            from: `${viewBoxHeight}`,
+            to: "0",
+            fill: "freeze",
+            calcMode: "spline",
+            keySplines: "0.1 0.8 0.2 1;0.1 0.8 0.2 1",
+            keyTimes: "0;0.5;1",
+          })
+      );
+    });
+    return [...horizontal, ...vertical];
+  }
+
+  render() {
+    const viewBoxWidth = 200;
+    const viewBoxHeight = 100;
+    const width = 600;
+    const height = 400;
+    const borderColor = "grey";
+    const horizontalUnit = 10;
+    const verticalUnit = 12;
+    const divideLineColor = "rgba(0, 0, 0, 0.2)";
+    const duration = 2;
+
+    const $border = this.makeBorder({ viewBoxWidth, viewBoxHeight, borderColor });
+    const $divideLines = this.makeDivideLine({
+      viewBoxWidth,
+      viewBoxHeight,
+      horizontalUnit,
+      verticalUnit,
+      divideLineColor,
+      duration,
+    });
     const lineGraph = createElement(
       h("svg", {
-        viewBox: `0 0 ${width} ${height}`,
+        viewBox: `0 0 ${viewBoxWidth} ${viewBoxHeight}`,
         xmlns: "http://www.w3.org/2000/svg",
         overflow: "visible",
-        width: widthLength,
-        height: heightLength,
-      })
+        width: width,
+        height: height,
+      }),
+      $border,
+      ...$divideLines
     );
 
     this.$target = new Svg("div", { class: "line-graph-container" }, lineGraph);
