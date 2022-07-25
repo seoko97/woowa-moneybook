ce7bc2a363fba9429697a3d0ef55c67710c0efea
jintak0401 <jintak0401@naver.com>
2022-07-26 08:09:52 +0900

    feat: 카테고리 관련 상수들 추가 #27
    - CATEGORY_KOR: 카테고리 영문을 한글로 바꿔주는 객체입니다.
    - CATEGORY_COLORS: 카테고리별 색깔을 구하는 객체입니다.

---
+++ frontend/src/constants/category.js
@@ -0,0 +1,27 @@
+const CATEGORY_KOR = {
+  eat: "식비",
+  life: "생활",
+  shopping: "쇼핑/뷰티",
+  traffic: "교통",
+  health: "의료/건강",
+  culture: "문화/여가",
+  unclassified: "미분류",
+  salary: "월급",
+  pinMoney: "용돈",
+  etcIncome: "기타수입",
+};
+
+const CATEGORY_COLORS = {
+  life: "#4a6cc3",
+  eat: "#4ca1de",
+  traffic: "#94d3cc",
+  shopping: "#4cb8b8",
+  health: "#6ed5eb",
+  culture: "#d092e2",
+  unclassified: "#817dce",
+  salary: "#b9d58c",
+  pinMoney: "#e6d267",
+  etcIncome: "#e2b765",
+};
+
+export { CATEGORY_KOR, CATEGORY_COLORS };
