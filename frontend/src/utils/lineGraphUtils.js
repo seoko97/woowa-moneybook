e4cc34f076bcabbd97e05cca61bcd8db701a75ee
jintak0401 <jintak0401@naver.com>
2022-07-26 11:24:02 +0900

    feat: 꺾은선 그래프의 하한선과 상한선의 값을 정해주는 함수 구현 #27
    - 6개월 치 데이터의 total 값에 따라 하한선과 상한선의 값이 달라져야 합니다.
    - 상한선과 하한선은 그래프의 border에 해당하는 값을 의미합니다.
    - 최소값이 0인 경우 가장 밑의 선(border 위)에 점이 위치해야 하여 lowerBoundary=0 으로, 상한선은 max보다 격자간격 하나만큼 더 큰 값과 10,000 중 더 큰 값이 됩니다.
    - 차이가 없는 경우 값을 중앙에 두고 위아래로 10,000만큼의 간격을 가집니다.
    - 차이가 있는 경우, 하한선은 최소금액보다 격자간격 하나만큼 적고, 상한선은 최대금액보다 격자간격 하나만큼 많습니다.

--- frontend/src/utils/lineGraphUtils.js
+++ frontend/src/utils/lineGraphUtils.js
@@ -15,4 +15,32 @@ const makeFullDataArray = ({ data, month: curMonth }) => {
   return ret;
 };
 
-export { makeFullDataArray };
+const makeBoundary = ({ data, gapUnit = 5 }) => {
+  let max = -1,
+    min = Infinity;
+  data.for(({ amount }) => {
+    if (max < amount) max = amount;
+    if (amount < min) min = amount;
+  });
+  let lowerBoundary = 0;
+  let upperBoundary = 0;
+  let gap = (max - min) / (gapUnit - 2);
+  const diff = max - min;
+  // 최소값이 0 인경우 -> 가장 밑의 선을 0으로
+  if (min === 0) {
+    upperBoundary = max + gap;
+    return [lowerBoundary, Math.max(upperBoundary, 10000)];
+  }
+  // 차이가 있는 경우 min을 밑에서 1번째, max를 위에서 1번째로
+  else if (diff !== 0) {
+    lowerBoundary = min - gap;
+    upperBoundary = max + gap;
+    return [lowerBoundary, upperBoundary];
+  }
+  // 차이가 없는 경우 값을 가운데로
+  else {
+    return [min - 10000, min + 10000];
+  }
+};
+
+export { makeFullDataArray, makeBoundary };
