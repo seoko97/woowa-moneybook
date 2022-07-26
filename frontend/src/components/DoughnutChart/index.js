063822c0130e0c59ea917087ab0f6b4e07a654f0
jintak0401 <jintak0401@naver.com>
2022-07-27 07:09:51 +0900

    feat: 도넛 차트 박스 오른쪽 리스트 구현
    - 헤더에 이번 달 지출 금액을 표시하여 줍니다.
    - 내용으로는 각 비율/금액의 내림차순으로 정렬되어 각 카테고리별 설명이 있습니다.

--- frontend/src/components/DoughnutChart/index.js
+++ frontend/src/components/DoughnutChart/index.js
@@ -1,8 +1,12 @@
-import './style.css';
+import "./style.css";
 import DoughnutChart from "./DoughnutChart";
 import Component from "../../core/component";
 import { createElement, h } from "../../utils/domHandler";
-import './style.css';
+import "./style.css";
+import { getState } from "../../core/store";
+import { doughnutState } from "../../store/doughnutState";
+import { getTotalAmount, getTotalPercent } from "../../utils/doughnutGraphUtils";
+import CategoryTag from "../CategoryTag";
 
 export default class DoughnutChartBox extends Component {
   constructor() {
@@ -14,14 +18,39 @@ export default class DoughnutChartBox extends Component {
     return this.$target;
   }
 
+  makeCategoryList() {
+    const { categoryExpenditureList: dataset } = getState(doughnutState);
+    const totalPercent = getTotalPercent(dataset);
+    return h(
+      "ul",
+      { class: "category-list" },
+      h(
+        "h2",
+        { class: "category-list--title" },
+        `이번 달 지출 금액 ${getTotalAmount(dataset).toLocaleString()}`
+      ),
+      dataset.map(({ total, category: name }, i) => {
+        return h(
+          "li",
+          { class: "category-list--li" },
+          h("div", { class: "category-li--label" }, new CategoryTag({ name })),
+          h("p", { class: "category-li--percent" }, `${Math.round(totalPercent[i] * 100)}%`),
+          h("p", { class: "category-li--amount" }, total.toLocaleString())
+        );
+      })
+    );
+  }
+
   render() {
+    const $categoryList = this.makeCategoryList();
     const $target = createElement(
       h(
         "div",
         {
           class: "doughnut-box",
         },
-        new DoughnutChart()
+        new DoughnutChart(),
+        $categoryList
       )
     );
 
