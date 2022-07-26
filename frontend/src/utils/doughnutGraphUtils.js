4166830c9db60f1ce037420418da0af98f468871
jintak0401 <jintak0401@naver.com>
2022-07-26 23:21:32 +0900

    feat: <path>내 호를 그려주는 d 값을 생성하는 함수 구현 #32
    - 호의 크기에 따라 적절히 호를 그려주는 <path>의 d값을 생성해주는 getCategoryDataPath 함수를 구현했습니다.

--- frontend/src/utils/doughnutGraphUtils.js
+++ frontend/src/utils/doughnutGraphUtils.js
@@ -5,3 +5,7 @@ const getCoordinatesForPercent = (percent) => {
   const y = RADIUS * Math.sin(2 * Math.PI * percent);
   return [x, y];
 };
+
+const getCategoryDataPath = ({ startX, startY, endX, endY, isLargeArcFlag }) => {
+  return `M ${startX} ${startY} A ${RADIUS} ${RADIUS} 0 ${isLargeArcFlag} 1 ${endX} ${endY} L 0 0`;
+};
