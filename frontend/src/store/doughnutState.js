5e01b8796da4d13b1d5e2a625470a22b217c6bc8
jintak0401 <jintak0401@naver.com>
2022-07-27 07:04:11 +0900

    fix: categoryExpenditureList의 amount를 total로 수정 #34
    - api 명세상 amount가 아닌 total을 넘겨받습니다. 이를 위해 amount에서 total로 수정하였습니다.

--- frontend/src/store/doughnutState.js
+++ frontend/src/store/doughnutState.js
@@ -4,13 +4,13 @@ export const doughnutState = initState({
   key: "doughnutState",
   defaultValue: {
     categoryExpenditureList: [
-      { category: "미분류", amount: 1000000 },
-      { category: "의료/건강", amount: 400000 },
-      { category: "문화/여가", amount: 400000 },
-      { category: "교통", amount: 300000 },
-      { category: "쇼핑/뷰티", amount: 300000 },
-      { category: "식비", amount: 200000 },
-      { category: "생활", amount: 100000 },
+      { category: "미분류", total: 1000000 },
+      { category: "의료/건강", total: 400000 },
+      { category: "문화/여가", total: 400000 },
+      { category: "교통", total: 300000 },
+      { category: "쇼핑/뷰티", total: 300000 },
+      { category: "식비", total: 200000 },
+      { category: "생활", total: 100000 },
     ],
   },
 });
