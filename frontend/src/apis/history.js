1a0214ff3fe18ee025293a06b34ee94fae9f5edd
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-07-27 08:51:40 +0900

    feat: histroy CRUD 로직 작성 #36

--- frontend/src/apis/history.js
+++ frontend/src/apis/history.js
@@ -1,9 +1,23 @@
 import { request } from ".";
+import { getQueryParams } from "../utils/getQueryParams";
 
 const requestCreateHistory = async (body) => {
-  const data = await request("/history", "POST", body);
+  const data = await request("/history", "POST", { ...body, userId: 1 });
 
-  console.log(data);
+  return data;
 };
 
-export { requestCreateHistory };
+const requestUpdateHistory = async (body) => {
+  const data = await request("/history", "PATCH", { ...body, userId: 1 });
+
+  return data;
+};
+
+const requestGetHistories = async (query) => {
+  const queryParams = getQueryParams(query);
+  const data = await request(`/history?${queryParams}`, "GET");
+
+  return data;
+};
+
+export { requestCreateHistory, requestGetHistories, requestUpdateHistory };
