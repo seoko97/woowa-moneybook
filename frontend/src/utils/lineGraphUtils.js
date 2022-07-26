d5b4e0868f76569b1ca63d226e5250a34406cf89
jintak0401 <jintak0401@naver.com>
2022-07-26 17:22:47 +0900

    feat: 가격표시 텍스트가 올바르게 위치하도록 align과 dy를 계산해주는 함수 구현 #27
    - 왼쪽 끝의 경우 그래프의 기울기가 / 인 경우 하단, \인 경우 상단에 가격표시가 나오도록 합니다.
    - 오른쪽 끝의 경우 그래프의 기울기가 \ 인 경우 하단, /인 경우 상단에 위치하도록 합니다.
    - 중간의 경우 V, \_, _/ 인경우 하단, ^, /-, \- 인 경우 상단에 위치하도록 합니다.
    - 그래프의 최하단과 가까운 경우에는 상단에 위치하도록 합니다.

--- frontend/src/utils/lineGraphUtils.js
+++ frontend/src/utils/lineGraphUtils.js
@@ -8,10 +8,10 @@ const makeFullDataArray = ({ data, month: curMonth }) => {
   const ret = Array.from({ length: MONTH_UNIT }, (_, i) => {
     let month = curMonth - i;
     if (month <= 0) month += 12;
-    month2idx[month] = idx;
+    month2idx[month] = idx++;
     return { month, total: 0 };
   });
-  data.forEach(({ month, total }) => (ret[month2idx[month]] = total));
+  data.forEach(({ month, total }) => (ret[month2idx[month]].total = total));
   return ret;
 };
 
@@ -22,26 +22,25 @@ const makeBoundary = ({ data, gapUnit = 5 }) => {
     if (max < total) max = total;
     if (total < min) min = total;
   });
-  let lowerBoundary = 0;
-  let upperBoundary = 0;
+  let lowerBoundary, upperBoundary;
   let gap = (max - min) / (gapUnit - 2);
   const diff = max - min;
   // 최소값이 0 인경우 -> 가장 밑의 선을 0으로
   if (min === 0) {
-    upperBoundary = max + gap;
-    return [lowerBoundary, Math.max(upperBoundary, 10000)];
+    lowerBoundary = 0;
+    upperBoundary = Math.max(max + gap, 10000);
   }
   // 차이가 있는 경우 min을 밑에서 1번째, max를 위에서 1번째로
   else if (diff !== 0) {
     lowerBoundary = min - gap;
-    if (lowerBoundary < 0) lowerBoundary = min - lowerBoundary / 100;
     upperBoundary = max + gap;
-    return [lowerBoundary, upperBoundary];
   }
   // 차이가 없는 경우 값을 가운데로
   else {
-    return [min - 10000, min + 10000];
+    lowerBoundary = min - 10000;
+    upperBoundary = max + 10000;
   }
+  return [lowerBoundary, upperBoundary];
 };
 
 const getCoordinates = ({ data, lowerBoundary, upperBoundary, viewBoxWidth, viewBoxHeight }) => {
@@ -53,4 +52,54 @@ const getCoordinates = ({ data, lowerBoundary, upperBoundary, viewBoxWidth, view
   });
 };
 
-export { makeFullDataArray, makeBoundary, getCoordinates };
+const calcGradient = (p1, p2) => {
+  return (p1[1] - p2[1]) / (p1[0] - p2[0]);
+};
+
+// 기울기에 따라 라벨 위치를 정해주는 함수
+const decideLabelPos = (grad1, grad2) => {
+  if (!grad1) {
+    return grad2 > 0 ? "down" : "up";
+  } else if (!grad2) {
+    return grad1 > 0 ? "up" : "down";
+  } else {
+    // ㅅ 모양
+    if (grad1 > 0 && grad2 < 0) return "up";
+    // V 모양
+    else if (grad1 < 0 && grad2 > 0) return "down";
+    // -\ 모양
+    else if (grad1 < 0 && grad2 < grad1) return "up";
+    // \_ 모양
+    else if (grad2 < 0 && grad1 < grad2) return "down";
+    // _/ 모양
+    else if (grad1 > 0 && grad1 < grad2) return "down";
+    // /- 모양
+    else return "up";
+  }
+};
+
+// 가격 텍스트의 align 위치와 dy 오프셋을 구해주는 함수
+const movePointsOffset = (pointsCoord, viewBoxHeight) => {
+  const position = { up: "7", down: "-2" };
+  const ret = [];
+  let grad;
+  for (let i = 0; i < pointsCoord.length; i++) {
+    if (i === 0) {
+      grad = calcGradient(pointsCoord[0], pointsCoord[1]);
+      ret.push(["start", position[decideLabelPos(undefined, grad)]]);
+    } else if (i === pointsCoord.length - 1) {
+      grad = calcGradient(pointsCoord[pointsCoord.length - 2], pointsCoord[pointsCoord.length - 1]);
+      ret.push(["end", position[decideLabelPos(grad, undefined)]]);
+    } else {
+      const grad1 = calcGradient(pointsCoord[i - 1], pointsCoord[i]);
+      const grad2 = calcGradient(pointsCoord[i], pointsCoord[i + 1]);
+      ret.push(["middle", position[decideLabelPos(grad1, grad2)]]);
+    }
+  }
+  ret.forEach((_, i) => {
+    if (pointsCoord[i][1] > viewBoxHeight - 8) ret[i][1] = [position["down"]];
+  });
+  return ret;
+};
+
+export { makeFullDataArray, makeBoundary, getCoordinates, movePointsOffset };
