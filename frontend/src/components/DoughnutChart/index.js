b36119a2ab25fd638db10eb0668184943431d715
jintak0401 <jintak0401@naver.com>
2022-07-26 23:29:07 +0900

    feat: 각 도넛 조각을 그려주는 makeParts 함수 구현 #32
    - DoughnutChart 클래스 내에 각 도넛의 조각을 그려주는 path들을 만들어 배열로 return해주는 makeParts 함수를 구현하였습니다.
    - 각 조각은 애니메이션을 가지며 전 조각이 모두 그려지면 다음 조각이 그려집니다.

--- frontend/src/components/DoughnutChart/index.js
+++ frontend/src/components/DoughnutChart/index.js
@@ -1,6 +1,17 @@
 import Component from "../../core/component";
+import { getState } from "../../core/store";
+import { doughnutState } from "../../store/doughnutState";
 import { createElement, h } from "../../utils/domHandler";
+import { CATEGORY_COLORS } from "../../constants/category";
 import Svg from "../../core/svg";
+import { getDoughnutChartPaths, getTotalPercent } from "../../utils/doughnutGraphUtils";
+import {
+  RADIUS,
+  ANIMATION_DURATION,
+  ANIMATION_SPEED,
+  INITIAL_DASH_OFFSET,
+  STROKE_WIDTH,
+} from "../../constants/doughnutChart";
 import "./style.css";
 
 export default class DoughnutChart extends Component {
@@ -12,13 +23,56 @@ export default class DoughnutChart extends Component {
     return this.$target;
   }
 
+  makeParts({ dataset, totalPercent, paths }) {
+    let accDuration = 0;
+    return dataset.map(({ category }, i) => {
+      const targetRad = 2 * Math.PI * totalPercent[i] * RADIUS;
+      const targetRestRad = 2 * Math.PI * (1 - totalPercent[i]) * RADIUS;
+      const dasharray = `${targetRad} ${targetRestRad}`;
+      const duration = ANIMATION_DURATION * (100 / ANIMATION_SPEED) * totalPercent[i];
+
+      const animate = h("animate", {
+        attributeName: "stroke-dashoffset",
+        begin: `${accDuration}`,
+        from: `${targetRad}`,
+        to: `${RADIUS * INITIAL_DASH_OFFSET}`,
+        dur: `${duration}`,
+        fill: "freeze",
+      });
+      accDuration += duration;
+      return h(
+        "path",
+        {
+          class: "doughnut--part",
+          d: paths[i],
+          fill: "transparent",
+          stroke: CATEGORY_COLORS[category],
+          "stroke-dasharray": dasharray,
+          "stroke-width": STROKE_WIDTH,
+          "stroke-dashoffset": targetRad,
+        },
+        animate
+      );
+    });
+  }
+
   render() {
+    const { categoryExpenditureList: dataset } = getState(doughnutState);
+    const totalPercent = getTotalPercent(dataset);
+    const paths = getDoughnutChartPaths(totalPercent, RADIUS);
+
+    const $parts = this.makeParts({ dataset, totalPercent, paths });
+
     const $doughnut = createElement(
-      h("svg", {
-        viewBox: "-15 -15 30 30",
-        xmlns: "http://www.w3.org/2000/svg",
-        class: "doughnut",
-      })
+      h(
+        "svg",
+        {
+          viewBox: "-15 -15 30 30",
+          xmlns: "http://www.w3.org/2000/svg",
+          class: "doughnut",
+        },
+        ...$parts
+      )
     );
     this.$target = new Svg("div", { class: ["doughnut-container"] }, $doughnut);
   }
