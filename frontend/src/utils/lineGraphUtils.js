c3f574cd3679a3b10dcb283e5cfa504afe90c03e
jintak0401 <jintak0401@naver.com>
2022-07-28 12:23:16 +0900

    fix: 12개월의 array로 만들어주는 함수의 에러 해결
    - 순서가 역순으로 되어있던 오류를 해결했습니다.

--- frontend/src/utils/lineGraphUtils.js
+++ frontend/src/utils/lineGraphUtils.js
@@ -7,19 +7,26 @@ import {
   VIEW_BOX_HEIGHT,
   VIEW_BOX_WIDTH,
 } from "../constants/lineGraph";
+import { makeYearMonthToStr } from "./dateHandler";
 
 // 6개월치 데이터에 존재하는 달이 6개 미만인 경우 6개로 만들어주는 함수
-const makeFullDataArray = ({ data, month: curMonth }) => {
+const makeFullDataArray = ({ data, month: curMonth, year: curYear }) => {
   if (data.length === MONTH_UNIT) return data;
-  const month2idx = {};
-  let idx = 0;
-  const ret = Array.from({ length: MONTH_UNIT }, (_, i) => {
-    let month = curMonth - i;
-    if (month <= 0) month += 12;
-    month2idx[month] = idx++;
-    return { month, total: 0 };
-  });
-  data.forEach(({ month, total }) => (ret[month2idx[month]].total = total));
+  const yearMonth2idx = {};
+  let idx = MONTH_UNIT - 1;
+  curMonth++;
+  const ret = Array.from({ length: MONTH_UNIT }, () => {
+    curMonth--;
+    if (curMonth === 0) {
+      curMonth = 12;
+      curYear--;
+    }
+    yearMonth2idx[makeYearMonthToStr(curYear, String(curMonth).padStart(2, "0"))] = idx--;
+    return { year: curYear, month: curMonth, total: 0 };
+  }).reverse();
+  data.forEach(
+    ({ year, month, total }) => (ret[yearMonth2idx[makeYearMonthToStr(year, month)]].total = total)
+  );
   return ret;
 };
 
